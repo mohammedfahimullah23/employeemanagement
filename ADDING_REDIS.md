ef085229201337d372988bda1559c7bd5b15b49c
Mohammed Fahimullah A <mohammedfahimullah23031998@gmail.com>
2026-01-08 23:35:57 +0530

    adding redis

--- ADDING_REDIS.md
+++ ADDING_REDIS.md
@@ -5,3 +5,6 @@ You need to add password in your app_settings for your app service
 
 Please note one thing I have selected enterprise redis edition, for this u can do only password username authenthication.
 This is a limitation from redis side for enterprise version.
+
+## Note
+I have noticed one thing that is we cannot automatically store the redis primary or secondary keys in the key vault.
--- src/main/java/com/example/learningjava/config/RedisConfig.java
+++ src/main/java/com/example/learningjava/config/RedisConfig.java
@@ -4,6 +4,7 @@ package com.example.learningjava.config;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
@@ -27,11 +28,20 @@ public class RedisConfig {
     @Value("${spring.redis.password}")
     private String redisPassword;
 
-    @Bean
-    public LettuceClientConfiguration lettuceClientConfiguration() {
+   @Bean
+    @Profile("dev")
+    public LettuceClientConfiguration devLettuceConfig() {
         return LettuceClientConfiguration.builder()
                 .useSsl()
-                .disablePeerVerification() // DEV ONLY
+                .disablePeerVerification()
+                .build();
+    }
+
+    @Bean
+    @Profile("prod")
+    public LettuceClientConfiguration prodLettuceConfig() {
+        return LettuceClientConfiguration.builder()
+                .useSsl()
                 .build();
     }
 
